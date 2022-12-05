f91743ba003fbeca453d03514463974670194b31
HelgaDp <106965471+HelgaDp@users.noreply.github.com>
2022-12-05 20:29:57 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -65,7 +65,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "28feed74ca8e6f8450fa975c80fe27e3";
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
